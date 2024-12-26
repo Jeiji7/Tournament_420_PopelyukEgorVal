8eaaf7e48a94a07f923b42750d047b67ac43e379
Egor <egorpopelyuk43@gmail.com>
2024-12-26 15:17:03 +0300

    создал страницу с созданием турниров

--- Tournament_420_PopelyukEgorVal/Pages/AddTournament.xaml.cs
+++ Tournament_420_PopelyukEgorVal/Pages/AddTournament.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tournament_420_PopelyukEgorVal;
+using Tournament_420_PopelyukEgorVal.DataBaseFolder;
 
 namespace Tournament_420_PopelyukEgorVal.Pages
 {
@@ -23,8 +27,27 @@ namespace Tournament_420_PopelyukEgorVal.Pages
         public AddTournament(User userOrganizer)
         {
             InitializeComponent();
+            var typee = App.db.Format.ToList();
+            FormatCB.ItemsSource = typee.ToList();
+            FormatCB.DisplayMemberPath = "Name";
+        }
+        private void Button_Click_Save(object sender, RoutedEventArgs e)
+        {
+            Tournament tournament = new Tournament();
+
+            tournament.NameTournament = NameTournamentTB.Text;
+            tournament.Prize = PrizeTB.Text;
+            tournament.Format = (Format)FormatCB.SelectedItem;
+            tournament.Count_user = int.Parse(CountUser.Text);
+            tournament.ID_user = App.currentUser.ID_user;
+            App.db.SaveChanges();
+            NavigationService.Navigate(new Pages.OrganizerPageMenu(App.currentUser));
+
         }
 
-
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.OrganizerPageMenu(App.currentUser));
+        }
     }
 }
